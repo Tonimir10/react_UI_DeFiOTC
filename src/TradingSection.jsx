099a5ded528f0055572d0ea33a78ad8382d82bdc
Francisco García <g.garciafran201@gmail.com>
2025-05-06 16:43:48 +0000

    Creacion De TradingCard y TradingSection

---
+++ src/TradingSection.jsx
@@ -0,0 +1,23 @@
+import './TradingSection.css';
+import TradingCard from './TradingCard/Card.jsx';
+
+function TradingDiv() {
+  return (
+    <div className="container">
+      <div className="text">
+        <p>
+          Take part in public, private or crowdfill deals. Negotiate private one-to-one deals with our secure chat. Setup your private deal and invite your partner via secure
+          sharable link your private deal and invite your partner via secure sharable link
+        </p>
+        <span>PERSON PERSON</span>
+        <span>0101</span>
+      </div>
+
+      <div className="card-container">
+        <TradingCard />
+      </div>
+    </div>
+  );
+}
+
+export default TradingDiv;
